fed8186d1451e063bf54b1c2b1f7e2044d6bf14d
anggawiguna1 <angga065109205@gmail.com>
2022-05-29 09:23:52 +0700

    Proses data

--- index.js
+++ index.js
@@ -23,19 +23,19 @@ function lakukanLooping(arrPegawai) {
 
       Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
   */
-  let hasilLooping = null;
+  let hasilLooping = [];
 
   /*
     TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
       yang berisi jumlah pria dari masing masing pegawai
   */
-  let jumlahPria = null;
+  let jumlahPria = 0;
 
   /*
     TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
       yang berisi jumlah wanita dari masing masing pegawai
   */
-  let jumlahWanita = null;
+  let jumlahWanita = 0;
 
   /*
     TODO 4: Buatlah sebuah variabel bernama "komentar"
@@ -49,6 +49,23 @@ function lakukanLooping(arrPegawai) {
         "Jumlah Pria dan Wanita berimbang"
   */
   let komentar = null;
+  for(let i = 0; i < arrPegawai.length; i++) {
+      hasilLooping.push(dataYangAkanDilooping[i].namaDepan + " " + (dataYangAkanDilooping[i].namaBelakang))
+      
+      if (dataYangAkanDilooping[i].jenisKelamin == "M") {
+        jumlahPria++;
+      } else {
+        jumlahWanita++;
+      }
+  }
+
+  if (jumlahPria > jumlahWanita) {
+    komentar = "Jumlah Pria lebih banyak dari Wanita"
+  }else if (jumlahPria < jumlahWanita) {
+    komentar = "Jumlah Wanita lebih banyak dari Pria"
+  }else {
+    komentar = "Jumlah Pria dan Wanita berimbang"
+  }
 
   // ! JANGAN DIMODIFIKASI
   return {
